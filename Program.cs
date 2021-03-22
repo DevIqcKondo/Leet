50040a7fb8ac1964e78bb788629e5df56da09a2b
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-22 16:26:28 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string convLeet;
+            
+            Console.WriteLine("Digite seu nome para iniciar.");
+            Console.WriteLine("_____________________________");
+            Console.Write("Nome: ");
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            nome = Console.ReadLine();
+            Console.ResetColor();
+            Console.WriteLine(""+nome+", digite uma frase: ");
+            
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            convLeet=Console.ReadLine();
+            Console.ResetColor();
+            Console.WriteLine(convLeet.Replace("o","0").Replace("O","0").Replace("e","3").Replace("E","3").Replace("I","1").Replace("i","1").Replace("L","1").Replace("l","1").Replace("a","4").Replace("A","4").Replace("T","7").Replace("t","7").Replace("S","5").Replace("s","5"));
+            Console.Beep();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Leet
-aça um tradutor simples para a linguagem Leet Speak.
+Faça um tradutor simples para a linguagem Leet Speak.
+Entrada	Saída
+A ou a	4
+E ou e	3
+I ou i ou L ou l	1
+O ou o	0
+T ou t	7
+S ou s	5
